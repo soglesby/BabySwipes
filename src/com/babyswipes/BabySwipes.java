ee8e947e9635373e58268eb32b22068d0b86ae0b
kathee <kathleenbarrera@gmail.com>
2012-11-26 05:55:33 -0800

    remove the write tag button and remove the clearDB from the main page

--- src/com/babyswipes/BabySwipes.java
+++ src/com/babyswipes/BabySwipes.java
@@ -25,11 +25,6 @@ public class BabySwipes extends Activity implements OnClickListener {
 	private static final String TAG = "BabySwipes";
 	Button trainingButton;
 	Button dataButton;
-	private boolean mInWriteMode;
-
-	private NfcAdapter mAdapter;
-	private TextView mTextView;
-	private Button mWriteTagButton;
 	
     private BabySwipesDB mDataBase;
     private TextView myText = null;
@@ -43,16 +38,6 @@ public class BabySwipes extends Activity implements OnClickListener {
 		trainingButton.setOnClickListener(this);
 		dataButton = (Button) findViewById(R.id.dataButton);
 		dataButton.setOnClickListener(this);
-
-		// grab our NFC Adapter
-		mAdapter = NfcAdapter.getDefaultAdapter(this);
-
-		// button that starts the tag-write procedure
-		mWriteTagButton = (Button) findViewById(R.id.write_tag_button);
-		mWriteTagButton.setOnClickListener(this);
-
-		// TextView that we'll use to output messages to screen
-		mTextView = (TextView) findViewById(R.id.text_view);
 		
         Log.v(TAG, "START");
         
@@ -64,25 +49,7 @@ public class BabySwipes extends Activity implements OnClickListener {
         
         // Example Database Usage
         mDataBase = new BabySwipesDB(getBaseContext());
-        
-        mDataBase.clearAllData();              // only use if you want to wipe the data
-
-        mDataBase.addTagType("Feeding");
-        mDataBase.addTagType("Medicine");
-        mDataBase.addTagType("Diaper");
-
-        mDataBase.addSwipe("Feeding", 100);
-        mDataBase.addSwipe("Feeding", 200);
-        mDataBase.addSwipe("Feeding", 300);
-        mDataBase.addSwipe("Feeding", 400);
-
-        mDataBase.addSwipe("Medicine", 250);
-        mDataBase.addSwipe("Medicine", 500);
-
-        mDataBase.addSwipe("Diaper", 150);
-        mDataBase.addSwipe("Diaper", 300);
-        mDataBase.addSwipe("Diaper", 450);
-        
+                
         int numRecent = 5;
         BabySwipesDB.BabySwipe[] bs = mDataBase.getRecentSwipes(numRecent);
         
@@ -91,112 +58,16 @@ public class BabySwipes extends Activity implements OnClickListener {
         {
         	Log.d(TAG, "tag: " + bs[i].tagName + ", time: " + bs[i].swipeTime);
         }
-        
-
+    
         int numTags = mDataBase.getNumberOfTags();
         
         TextView theText = (TextView)findViewById(R.id.numTagText2);
-        theText.setText("" + numTags);
+        theText.setText("Number of tags recorded : " + numTags);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		disableWriteMode();
-	}
-	/**
-	 * Force this Activity to get NFC events first
-	 */
-	private void enableWriteMode() {
-		mInWriteMode = true;
-		
-		// set up a PendingIntent to open the app when a tag is scanned
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-            new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        IntentFilter[] filters = new IntentFilter[] { tagDetected };
-        
-		mAdapter.enableForegroundDispatch(this, pendingIntent, filters, null);
-	}
-	
-	private void disableWriteMode() {
-		mAdapter.disableForegroundDispatch(this);
-	}
-
-	public void onNewIntent(Intent intent) {
-		if (mInWriteMode) {
-			mInWriteMode = false;
-			Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-			writeTag(tag);
-		}
-	}
-
-	/**
-	 * TODO DELETE
-	 * Temp code used to test the tag reading
-	 * @param tag
-	 * @return
-	 */
-	private boolean writeTag(Tag tag) {
-		// record to launch Play Store if app is not installed
-		NdefRecord appRecord = NdefRecord
-				.createApplicationRecord("com.babyswipes");
-
-		String activityName = "Diaper";
-
-		// record that contains our custom "retro console" game data, using
-		// custom MIME_TYPE
-		byte[] activity = activityName.getBytes();
-		byte[] mimeBytes = MimeType.NFC_MIME.getBytes(Charset.forName("US-ASCII"));
-		NdefRecord activityNdef = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,
-				mimeBytes, new byte[0], activity);
-		NdefMessage message = new NdefMessage(new NdefRecord[] { activityNdef,
-				appRecord });
-
-		try {
-			// see if tag is already NDEF formatted
-			Ndef ndef = Ndef.get(tag);
-			if (ndef != null) {
-				ndef.connect();
-
-				if (!ndef.isWritable()) {
-					displayMessage("Read-only tag.");
-					return false;
-				}
-
-				// work out how much space we need for the data
-				int size = message.toByteArray().length;
-				if (ndef.getMaxSize() < size) {
-					displayMessage("Tag doesn't have enough free space.");
-					return false;
-				}
-
-				ndef.writeNdefMessage(message);
-				displayMessage("Tag written successfully.");
-				return true;
-			} else {
-				// attempt to format tag
-				NdefFormatable format = NdefFormatable.get(tag);
-				if (format != null) {
-					try {
-						format.connect();
-						format.format(message);
-						displayMessage("Tag written successfully!");
-						return true;
-					} catch (IOException e) {
-						displayMessage("Unable to format tag to NDEF.");
-						return false;
-					}
-				} else {
-					displayMessage("Tag doesn't appear to support NDEF format.");
-					return false;
-				}
-			}
-		} catch (Exception e) {
-			displayMessage("Failed to write tag");
-		}
-
-		return false;
 	}
 
 	@Override
@@ -219,15 +90,8 @@ public class BabySwipes extends Activity implements OnClickListener {
 			i = new Intent(this, DataActivity.class);
 			startActivity(i);
 			break;
-		case R.id.write_tag_button:
-			displayMessage("Touch and hold tag against phone to write.");
-			enableWriteMode();
-			break;
 		}
 	}
 
-	private void displayMessage(String message) {
-		mTextView.setText(message);
-	}
 
 }
