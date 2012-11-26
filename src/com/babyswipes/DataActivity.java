1578788e5debf6cdd8481f683404aa21a38403bd
Stephen Oglesby <Stephen.Oglesby@gmail.com>
2012-11-25 21:23:11 -0800

    Integrated data graphing code from Gavin

--- src/com/babyswipes/DataActivity.java
+++ src/com/babyswipes/DataActivity.java
@@ -2,17 +2,29 @@ package com.babyswipes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 import android.support.v4.app.NavUtils;
 
-public class DataActivity extends Activity {
+public class DataActivity extends Activity implements OnItemClickListener {
+	
+	private static final String DATA_TAG = "DataActivity";
+	ListView tagTypeList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        
+        tagTypeList = (ListView) findViewById(R.id.tagTypeList);
+        tagTypeList.setOnItemClickListener(this);
     }
 
     @Override
@@ -30,6 +42,29 @@ public class DataActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+        Intent i = null;
+        Log.d(DATA_TAG, "tagType " + pos + " selected");
+        switch(pos) {
+        case 0: //Feedings
+            i = new Intent(this, FeedingGraph.class);    
+            break;
+        case 1: //Diapers
+            i = new Intent(this, DiaperGraph.class);
+            break;
+        case 2: //Nap
+            i = new Intent(this, NapGraph.class);
+            break;
+        case 3: //Medicine
+            i = new Intent(this, MedicineGraph.class);
+             break;    
+        }
+        if (i != null) 
+            startActivity(i);
     }
 
 }
